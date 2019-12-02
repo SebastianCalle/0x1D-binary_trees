4842dde6e3c66c20dda11b35f0d616d099ed0ec7
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-12-02 20:00:58 +0000

    [Feature]: Create function that measure size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - function that return height of a tree
+ * binary_tree_size - function that size of a tree
  * @tree: pointer to a tree
  * return: size of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-
 	size_t count = 0;
 
 	if (!tree)
 		return (0);
-	count = binary_tree_height(tree->left) + 1 + binary_tree_height(tree->right);
+	count = binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right);
 	return (count);
 }
