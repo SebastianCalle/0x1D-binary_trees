c8b610be7b3fadbe3957ffd1eaa1baf741e4853a
Karen Herrera <837@holbertonschool.com>
2019-12-02 18:00:25 +0000

    number 3

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 
-	binary_tree_t *node, *temp;
+	binary_tree_t *node = NULL, *temp = NULL;
 
 	if (!parent)
 		node = binary_tree_node(parent, value);
