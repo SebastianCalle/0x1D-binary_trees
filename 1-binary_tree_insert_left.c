b507a9005c5845b21e59b083b578940086ecaef3
Karen Herrera <837@holbertonschool.com>
2019-12-02 17:29:52 +0000

    number 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * *binary_tree_insert_left - insert a node.
+ * @parent: pointer to parent node
+ * @value: value of the node
+ * Return: a pointer to the created node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *node, *temp;
+
+	if (!parent)
+		node = binary_tree_node(parent, value);
+	else
+	{
+		if (!parent->left)
+		{
+			node = binary_tree_node(parent, value);
+			parent->left = node;
+			node->parent = parent;
+		}
+		else
+		{
+			node = binary_tree_node(parent, value);
+			temp = parent->left;
+			parent->left = node;
+			node->parent = parent;
+			node->left = temp;
+			temp->parent = node;
+		}
+	}
+	return (node);
+}
