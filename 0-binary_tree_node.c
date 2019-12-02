40c2d806029d88ef326ba56902786bf57a3e1e86
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-12-02 17:32:12 +0000

    [Feature]; change condition for null

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	node->n = value;
 	if (parent == NULL)
-		node->parent = node;
+		node->parent = NULL;
 	else
 		node->parent = parent;
 
